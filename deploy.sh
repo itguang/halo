9d9b125ca5ee4ec9bd9b95e58463b879294bbc7c
guang <guang@xingren.com>
2019-04-23 18:23:56 +0800

    更新 deploy.sh 失败的问题

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # 指定Halo的根目录，请按实际修改
-HALO_DIR="/usr/local/halo"
-Out
+HALO_DIR="."
 
 # 拉取最新的源码
 # git pull
@@ -11,7 +10,7 @@ Out
 cd $HALO_DIR
 
 # 停止Halo
-sh $HALO_DIR/target/dist/halo.sh stop
+sh $HALO_DIR/bin/halo.sh stop
 
 # 执行打包
 mvn package -Pprod
